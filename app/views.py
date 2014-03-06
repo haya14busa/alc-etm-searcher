946fc072b0fc6861cb188e6444b250dd2fb77b1d
haya14busa <hayabusa1419@gmail.com>
2014-03-07 03:57:07 +0900

    Add templates: layout.html & 404.html

--- app/views.py
+++ app/views.py
@@ -26,8 +26,14 @@
 #=============================================================================
 
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 def index():
-    return 'Happy Vimming!'
+    return render_template('index.html', title='Happy Vimming!')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
