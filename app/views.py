a2756098c0ddbb2b05747d3ddce0ad4c26dc6b40
haya14busa <hayabusa1419@gmail.com>
2014-03-10 16:36:25 +0900

    Add About page: contents is [WIP]

--- app/views.py
+++ app/views.py
@@ -41,6 +41,11 @@ def index():
                            is_found=is_found)
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
